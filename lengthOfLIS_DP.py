34b8b89662bf203be4acb3fdd733c59892934b4f
jzhao16 <jingzhaoestel@gmail.com>
2017-07-18 12:13:11 -0400

    Add LengthOfLIS_DP

---
+++ lengthOfLIS_DP.py
@@ -0,0 +1,8 @@
+class Solution(object):
+	def lengthOfLIS(self, nums):
+		dp = [1] * len(nums)
+		for i in range(1, len(nums)):
+			for j in range (0, i):
+				if nums[i] > nums[j]:
+					dp[i] = max(dp[i],dp[j]+1)
+		return max(dp) if dp else 0
